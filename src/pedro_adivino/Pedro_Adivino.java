cf9eb3005168bd1dac32d3fdbfeaaf5bf3d503d8
Usuario <Usuario@DESKTOP-UNSG7D8>
2019-10-29 13:47:02 +0100

    Version final

---
+++ src/pedro_adivino/Pedro_Adivino.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pedro_adivino;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Pedro_Adivino {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+ 
+        int n = 0,cont = 0;
+        int x = (int) ((Math.random()*(50-10)+10));
+        //System.out.print(x);
+        
+        Scanner sc = new Scanner(System.in);
+        do {
+            System.out.print("\nIntroduce el valor de X: ");
+            n = sc.nextInt();
+            if (n > x) {
+                System.out.print("Te pasaste Amigo");
+            } else if (n < x){
+                System.out.print("Casi cerca ...");
+            }
+            cont++;
+        } while (n != x);
+        System.out.print("Exelente!!! Haz acertado , No. de intentos: "+cont );
+        sc.close();
+    
+    }
+    
+}
